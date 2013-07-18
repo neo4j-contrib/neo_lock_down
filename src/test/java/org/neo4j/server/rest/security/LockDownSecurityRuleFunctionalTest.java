b58f69acefafa98ec03fe88bf02036ed0c6f2d51
Max De Marzi <maxdemarzi@hotmail.com>
2013-07-18 00:09:40 -0500

    trying with batch

--- src/test/java/org/neo4j/server/rest/security/LockDownSecurityRuleFunctionalTest.java
+++ src/test/java/org/neo4j/server/rest/security/LockDownSecurityRuleFunctionalTest.java
@@ -26,6 +26,29 @@ public class LockDownSecurityRuleFunctionalTest {
         server.stop();
     }
 
+
+    @Test
+    public void should401WithBasicChallengeWhenASecurityRuleFailswithBatch()
+            throws Exception
+    {
+        NeoServer server = ServerBuilder.server()
+                .withSecurityRules(LockDownSecurityRule.class.getCanonicalName() ).build();
+        server.start();
+
+        RestRequest restRequest = new RestRequest(server.baseUri());
+        String dummyText = "[ {\n" +
+                "  \"method\" : \"POST\",\n" +
+                "  \"to\" : \"/node/0/traverse\",\n" +
+                "  \"body\" : {\n" +
+                "    \"order\" : \"breadth_first\"\n" +
+                "  },\n" +
+                "  \"id\" : 0";
+        JaxRsResponse response = restRequest.post("db/data/batch", dummyText);
+        assertTrue(response.getStatus() == 401);
+        assertTrue(response.getHeaders().getFirst( "WWW-Authenticate" ).contains("Basic realm=\"WallyWorld\""));
+        server.stop();
+    }
+
     @Test
     public void should200WithBasicChallengeWhenASecurityRuleSucceeds()
             throws Exception
